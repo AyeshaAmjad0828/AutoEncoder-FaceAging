d87b038858aec2cd836b030c70750319bfb484de
A.Amjad <aamjad@khi.iba.edu.pk>
2024-01-01 00:11:48 +0500

    Updated train.py and added old face folder

--- Train.py
+++ Train.py
@@ -4,18 +4,64 @@ from tensorflow.keras.layers import Input, Conv2D, MaxPooling2D, UpSampling2D
 from tensorflow.keras.models import Model
 import cv2
 import os
+from PIL import Image
 
-folder_path = 'data/utkcropped'  
+Xfolder_path = 'data/young'  
+Yfolder_path = 'data/old'
+
 image_list = []
+image_dimensions = []  
 
 for filename in os.listdir(folder_path):
     if filename.endswith(".jpg") or filename.endswith(".png"):
         image_path = os.path.join(folder_path, filename)
-        image = cv2.imread(image_path)
+        image = Image.open(image_path)
         image_list.append(image)
 
-#utoencoder model
-input_img = Input(shape=(200, 200, channels))  
+        # Get image dimensions (width x height)
+        width, height = image.size
+        image_dimensions.append((width, height))
+
+# Display the image dimensions
+print("Image Dimensions:")
+for idx, dimensions in enumerate(image_dimensions, start=1):
+    print(f"Image {idx}: Width={dimensions[0]}, Height={dimensions[1]}")
+
+
+num_images = len(os.listdir(Xfolder_path))
+
+# Collect normalized images in a list
+Xnormalized_images = []
+Ynormalized_images = []
+
+for filename in os.listdir(Xfolder_path):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        Ximage_path = os.path.join(Xfolder_path, filename)
+        Ximage = Image.open(Ximage_path)
+        
+        # Normalize the image
+        Ximage_array = np.array(Ximage)
+        Xnormalized_image = Ximage_array / 255.0  # Normalizing to range [0, 1]
+        Xnormalized_images.append(Xnormalized_image)
+
+for filename in os.listdir(Yfolder_path):
+    if filename.endswith(".jpg") or filename.endswith(".png"):
+        Yimage_path = os.path.join(Yfolder_path, filename)
+        Yimage = Image.open(Yimage_path)
+        
+        # Normalize the image
+        Yimage_array = np.array(Yimage)
+        Ynormalized_image = Yimage_array / 255.0  # Normalizing to range [0, 1]
+        Ynormalized_images.append(Ynormalized_image)
+
+# Convert the list of normalized images to a NumPy array
+
+X_train = np.array(Xnormalized_images)
+Y_train = np.array(Ynormalized_images)
+
+
+#autoencoder model
+input_img = Input(shape=(200, 200, 3))  
 
 # Encoder layers
 x = Conv2D(32, (3, 3), activation='relu', padding='same')(input_img)
@@ -33,12 +79,20 @@ decoded = Conv2D(3, (3, 3), activation='sigmoid', padding='same')(x)  # Output i
 autoencoder = Model(input_img, decoded)
 autoencoder.compile(optimizer='adam', loss='mean_squared_error')
 
-# Normalizing the data between 0 and 1
-X_train = X_train.astype('float32') / 255.
-Y_train = Y_train.astype('float32') / 255.
 
 # Training
 autoencoder.fit(X_train, Y_train, epochs=50, batch_size=32, shuffle=True)
+
+
+
+test_image_path = 'path/to/your/image.jpg'  # Replace with your image path
+test_image = Image.open(test_image_path)
+
+# Display the image (optional)
+test_image.show()
+
+# Convert the image to a numpy array
+image_array = np.array(test_image)
 
 # Assuming you have a young face image (X_test)
 X_test = X_test.astype('float32') / 255.
